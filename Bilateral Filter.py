b82e19d7786ddcd6e44eaee843e76a377d61f6ab
Leith Osborne <osborneleith@gmail.com>
2020-06-05 08:37:58 +0100

    bilateral filter

---
+++ Bilateral Filter.py
@@ -0,0 +1,28 @@
+import cv2
+import argparse
+
+parser = argparse.ArgumentParser(description="Image(s) of interest")
+parser.add_argument('images', nargs='+')
+args = parser.parse_args()
+
+
+for image_str in args.images:
+    # read images from file
+    img = cv2.imread('images/' + image_str, cv2.IMREAD_COLOR)
+    if '.' in image_str:
+        split_image_str = image_str.rsplit('.', 1)[0]
+    else:
+        print("File type not specified.")
+        exit()
+    # check images have loaded
+    if img is not None:
+        # apply filter
+        bilateral_filter1 = cv2.bilateralFilter(img, 7, 30, 30)
+        bilateral_filter2 = cv2.bilateralFilter(img, 9, 60, 60)
+        # write images to file
+        cv2.imwrite('images/bilateral filter' + split_image_str + '_output1.png', bilateral_filter1)
+        cv2.imwrite('images/bilateral filter' + split_image_str + '_output2.png', bilateral_filter1)
+
+    else:
+        print("No image file successfully loaded.")
+
